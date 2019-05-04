5067100de1c6829abe40629fa6250e82e82d4147
Guto Xavier <gutoffline@gmail.com>
2019-05-03 22:26:31 -0300

    Criação de obstáculos

---
+++ Desafio nas Alturas/Assets/Scripts/Aviao.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour
+{
+    
+    Rigidbody2D fisica;
+    [SerializeField]
+    private float forca = 10f;
+    private void Awake()
+    {
+        //Debug.Log("Acordei");
+        this.fisica = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Impulsionar(){
+        this.fisica.AddForce(Vector2.up * forca, ForceMode2D.Impulse);
+    }
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            //Debug.Log("Clicou");
+            Impulsionar();
+        }
+    }
+}
---
+++ Desafio nas Alturas/Assets/Scripts/Obstaculos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculos : MonoBehaviour
+{
+    // Update is called once per frame
+    [SerializeField]
+    private float velocidade = 0.2f;
+    void Update()
+    {
+        transform.Translate(Vector3.left * velocidade);
+    }
+}
